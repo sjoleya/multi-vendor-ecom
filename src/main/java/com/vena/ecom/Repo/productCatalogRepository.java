002d09f855b34f6711bec467018aac9d9c2c6055
Sarthak Joleya <sjoleya@venacorp.com>
2025-05-20 15:33:47 +0530

    added exception handling and refactoring

--- src/main/java/com/vena/ecom/Repo/productCatalogRepository.java
+++ src/main/java/com/vena/ecom/Repo/productCatalogRepository.java
@@ -3,9 +3,8 @@ package com.vena.ecom.repo;
 import com.vena.ecom.model.ProductCatalog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Repository
-public interface productCatalogRepository extends JpaRepository<ProductCatalog,String> {
+public interface ProductCatalogRepository extends JpaRepository<ProductCatalog, String> {
 
 }
