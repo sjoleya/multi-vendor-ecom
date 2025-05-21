96d7abcfb83253af4c6845955f1e8a8ad4493513
Sarthak Joleya <sjoleya@venacorp.com>
2025-05-21 11:39:32 +0530

    added entity

---
+++ src/main/java/com/vena/ecom/model/Order.java
@@ -0,0 +1,130 @@
+package com.vena.ecom.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.vena.ecom.model.audit.Auditable;
+import com.vena.ecom.model.enums.OrderStatus;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Order extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId", referencedColumnName = "userId")
+    private User customer;
+
+    private LocalDateTime orderDate;
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus overallOrderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "shippingAddressId", referencedColumnName = "addressId")
+    private Address shippingAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "billingAddress", referencedColumnName = "addressId")
+    private Address billingAddress;
+
+    @OneToMany(mappedBy = "order")
+    private List<Order> orderItems;
+
+    @OneToOne(mappedBy = "order")
+    private Payment payment;
+
+    public Order() {
+    }
+
+    public Order(String orderId, User customer, LocalDateTime orderDate, BigDecimal totalAmount,
+            OrderStatus overallOrderStatus, Address shippingAddress, Address billingAddress, List<Order> orderItems,
+            Payment payment) {
+        this.orderId = orderId;
+        this.customer = customer;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.overallOrderStatus = overallOrderStatus;
+        this.shippingAddress = shippingAddress;
+        this.billingAddress = billingAddress;
+        this.orderItems = orderItems;
+        this.payment = payment;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public OrderStatus getOverallOrderStatus() {
+        return overallOrderStatus;
+    }
+
+    public void setOverallOrderStatus(OrderStatus overallOrderStatus) {
+        this.overallOrderStatus = overallOrderStatus;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public List<Order> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<Order> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
---
+++ src/main/java/com/vena/ecom/model/OrderItem.java
@@ -0,0 +1,106 @@
+package com.vena.ecom.model;
+
+import java.math.BigDecimal;
+
+import com.vena.ecom.model.enums.ItemStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "orderId", referencedColumnName = "orderId")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn
+    private VendorProduct vendorProduct;
+
+    private Integer quantity;
+    private BigDecimal priceAtPurchase;
+    private BigDecimal subtotal;
+
+    @Enumerated(EnumType.STRING)
+    private ItemStatus itemStatus;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(String orderItemId, Order order, VendorProduct vendorProduct, Integer quantity,
+            BigDecimal priceAtPurchase, BigDecimal subtotal, ItemStatus itemStatus) {
+        this.orderItemId = orderItemId;
+        this.order = order;
+        this.vendorProduct = vendorProduct;
+        this.quantity = quantity;
+        this.priceAtPurchase = priceAtPurchase;
+        this.subtotal = subtotal;
+        this.itemStatus = itemStatus;
+    }
+
+    public String getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(String orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public VendorProduct getVendorProduct() {
+        return vendorProduct;
+    }
+
+    public void setVendorProduct(VendorProduct vendorProduct) {
+        this.vendorProduct = vendorProduct;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPriceAtPurchase() {
+        return priceAtPurchase;
+    }
+
+    public void setPriceAtPurchase(BigDecimal priceAtPurchase) {
+        this.priceAtPurchase = priceAtPurchase;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public ItemStatus getItemStatus() {
+        return itemStatus;
+    }
+
+    public void setItemStatus(ItemStatus itemStatus) {
+        this.itemStatus = itemStatus;
+    }
+}
